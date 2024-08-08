9ff01affa2744528cfd257c69b55bc4335ce80d2
dyun23 <ozsally@naver.com>
2024-08-08 09:45:07 +0900

    [Feat] 사업자 상품 등록 기능 추가

---
+++ src/pages/ItemRegisterPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ItemRegisterComponent />
+    </div>
+</template>
+
+<script>
+    import ItemRegisterComponent from '../components/item/ItemRegisterComponent.vue'
+    export default {
+        components: {
+            ItemRegisterComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
